de2ef498948f2d2759d9db3c63026d38811994ca
Nikita <nikita0levchenko@gmail.com>
2019-12-27 20:55:07 +0300

    -_-

--- src/main/scala/FirstScalaClass.scala
+++ src/main/scala/FirstScalaClass.scala
@@ -1,13 +1,12 @@
 class FirstScalaClass {
-
-
+  val a = 8;
 }
 
 object FirstScalaClass {
   def helloName(name: String ) = "Hello " + Option(name).get
   def helloNameWithStringInterpolation(name: String) =  {
     val sName = Option(name).get
-    s"Hello${name}"
+    s"Hello${sName}"
   }
 
 }
